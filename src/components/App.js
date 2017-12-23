6eab5683c88d97d9e21ea887ce3330bc13691e32
Lexa Hall <lexahall@Lexas-MacBook-Air.local>
2017-12-23 10:56:15 -0800

    Set up Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter as Router} from 'react-router-dom'
+import HomePage from './HomePage';
+import SignInPage from './SignInPage';
+import BoardsPage from './BoardsPage';
+import BoardPage from './BoardPage';
+import NotFoundPage from './NotFoundPage';
 import Apollo from './Apollo';
 
 function App() {
   return (
     <Apollo>
-      <div>Hello World!</div>
+      <Router>
+        <Switch>
+            <Route exact path='/' component={HomePage}/>
+            <Route exact path='/signin' component={SignInPage}/>
+            <Route exact path='/boards' component={BoardsPage}/>
+            <Route path='/board/:id' component={BoardPage}/>
+            <Route component={NotFoundPage} />
+          </Switch>
+      </Router>
     </Apollo>
   )
 }
