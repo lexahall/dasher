9fbc5c7928b5573c45081c5d5ab27e0731b55e30
Lexa Hall <lexahall@Lexas-MacBook-Air.local>
2017-12-23 15:59:06 -0800

    Add sign in with github button

--- src/components/SignInPage.js
+++ src/components/SignInPage.js
@@ -1,11 +1,78 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import queryString from 'query-string';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import { GITHUB_CLIENT_ID, GRAPHCOOL_TOKEN_KEY } from '../constants';
 
-function SignInPage() {
-   return (
-      <div>
-         <h1>Sign In</h1>
-      </div>
-   );
+class SignInPage extends React.Component {
+   state = {
+      loading: false,
+      error: '',
+   };
+
+   componentDidMount() {
+      const query = queryString.parse(this.props.location.search);
+
+      if (query.code) {
+         this.signInUser(query.code);
+      }
+   }
+
+   signInUser = async githubCode => {
+      try {
+         this.setState({ loading:true });
+
+         const response = await this.props.authenticateUserMutation({
+            variables: { githubCode },
+         });
+
+         localStorage.setItem(
+            GRAPHCOOL_TOKEN_KEY,
+            response.data.authenticateUser.token,
+         );
+
+         this.props.history.replace('/');
+      } catch (error) {
+         this.setState({ loading: false, error });
+      }
+   };
+
+   goToGithubAuthPage = () => {
+      window.location = this.getGithubAuthUrl();
+   };
+
+   getGithubAuthUrl = () => {
+      const query = queryString.stringify({
+         client_id: GITHUB_CLIENT_ID,
+      });
+   };
+
+   render() {
+      if (localStorage.getItem(GRAPHCOOL_TOKEN_KEY)) {
+         return <Redirect to="/" />;
+      }
+
+      return (
+         <div>
+            <h1>Sign In</h1>
+            {this.state.loading && 'Loading'}
+            {this.state.error && 'Error'}
+            <button onClick={this.goToGithubAuthPage}>Sign in with GitHub</button>
+         </div>
+      );
+   }
 }
 
-export default SignInPage;
+const AUTHENTICATE_USER_MUTATION = gql`
+   mutation AuthenticateUserMutation($githubCode: String!) {
+      authenticateUser(githubCode: $githubCode) {
+         token
+      }
+   }
+
+`;
+
+export default graphql(AUTHENTICATE_USER_MUTATION, {
+   name: 'authenticateUserMutation',
+})(SignInPage);
