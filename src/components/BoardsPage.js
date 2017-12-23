e9a2d022855c3c722c44280d27cf186d782695e8
Lexa Hall <lexahall@Lexas-MacBook-Air.local>
2017-12-23 11:30:09 -0800

    Implement AllBoardsQuery

--- src/components/BoardsPage.js
+++ src/components/BoardsPage.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 
-function BoardsPage() {
+function BoardsPage({ AllBoardsQuery }) {
+   if (AllBoardsQuery.loading) {
+      return <div>Loading</div>;
+ }
+
+   if (AllBoardsQuery.error) {
+      console.error(AllBoardsQuery.error.stack);
+      return <div>Error</div>;
+  }
+
    return (
       <div>
          <h1>Boards</h1>
+         <ul>
+            {AllBoardsQuery.allBoards.map(board => (
+               <li key={board.id}>
+                  name={board.name}
+               </li>
+            ))}
+         </ul>
       </div>
    );
 }
 
-export default BoardsPage;
+const ALL_BOARDS_QUERY = gql`
+query AllBoardsQuery {
+   allBoards(orderBy: updatedAt_DESC) {
+      id
+      name
+   }
+}
+`
+
+export default graphql(ALL_BOARDS_QUERY, {name: 'AllBoardsQuery'}) (BoardsPage);
