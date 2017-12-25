aeae08a1a89058db1863bcf4633100916960aba9
Lexa Hall <lexahall@Lexas-MacBook-Air.local>
2017-12-24 17:50:00 -0800

    Add user account info to homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,38 +1,38 @@
 import React from 'react';
-import { graphql } from 'react-apollo'
-import gql from 'graphql-tag'
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import AccountMenu from './AccountMenu';
 
-function HomePage({ AllBoardsQuery }) {
-   if (AllBoardsQuery.loading) {
-      return <div>Loading</div>;
- }
-
-   if (AllBoardsQuery.error) {
-      console.error(AllBoardsQuery.error.stack);
-      return <div>Error</div>;
+function HomePage({ allBoardsQuery }) {
+  if (allBoardsQuery.loading) {
+    return <div>Loading</div>;
   }
 
-   return (
-      <div>
-         <h1>Boards</h1>
-         <ul>
-            {AllBoardsQuery.allBoards.map(board => (
-               <li key={board.id}>
-                  name={board.name}
-               </li>
-            ))}
-         </ul>
-      </div>
-   );
+  if (allBoardsQuery.error) {
+    console.error(allBoardsQuery.error.stack);
+    return <div>Error</div>;
+  }
+
+  return (
+    <div>
+      <AccountMenu />
+      <h1>Boards</h1>
+      <ul>
+        {allBoardsQuery.allBoards.map(board => (
+          <li key={board.id}>{board.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 const ALL_BOARDS_QUERY = gql`
-query AllBoardsQuery {
-   allBoards(orderBy: updatedAt_DESC) {
+  query AllBoardsQuery {
+    allBoards(orderBy: updatedAt_DESC) {
       id
       name
-   }
-}
-`
+    }
+  }
+`;
 
-export default graphql(ALL_BOARDS_QUERY, {name: 'AllBoardsQuery'}) (HomePage);
+export default graphql(ALL_BOARDS_QUERY, { name: 'allBoardsQuery' })(HomePage);
