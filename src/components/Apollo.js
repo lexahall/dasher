fdcf158d4c4edef69ecc8a46020d65eccb294955
Lexa Hall <lexahall@Lexas-MacBook-Air.local>
2017-12-24 16:14:47 -0800

    Configure Apollo Link

--- src/components/Apollo.js
+++ src/components/Apollo.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
+import { ApolloLink, concat } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { GRAPHCOOL_TOKEN_KEY } from '../constants';
 
 const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjbjnipex0xp901307o5nvkhk' })
 
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(GRAPHCOOL_TOKEN_KEY);
+  operation.setContext({
+    headers: {
+      authorization: token ? `Bearer ${token}` : null,
+    },
+  });
+
+  return forward(operation);
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: concat(authMiddleware, httpLink),
   cache: new InMemoryCache()
 });
 
